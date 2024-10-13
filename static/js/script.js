883415c6a4adca50dc9dd26bf72aff129bc92090
yooLucq <lucacoppolecchia07@gmail.com>
2024-10-13 20:20:05 +0200

    code update
    add function and improving code

--- static/js/script.js
+++ static/js/script.js
@@ -1,37 +1,51 @@
-function getRandomInt(min, max){
-    return Math.floor(Math.random() * (max - min + 1)) + min
+function getRandomInt(min = 1, max = 100){
+    let num = Math.random()
+    num = num * (max - min) + min
+    return Math.floor(num)
 }
 
-function indovinaNumero(){
-    let tentativi = 5
-    let numeroCasuale = getRandomInt(0,100)
-    let indovina
-    let i = 0
-    let j = 0 
+function sanitize(Var){
+    let san
+    if(!isNaN(Var)) {
+        san = parseInt(Var.trim());
+    }
+    else {
+        san = NaN;
+    }
+    return san
+}
 
-    alert("Indovina il numero tra 0 e 100")
-    alert("Hai " + tentativi + "tentativi")
+function handleClick(event) {
+    let input = document.querySelector("#guess").value;
+    numeroInserito = sanitize(input);
 
-    while(i < tentativi && j === 0){
-        indovina = parseInt(prompt("Tentativo "+ (i + 1) +":"))
-
-        if(indovina === numeroCasuale){
-            j = 1;
-        } 
-        else if (indovina < numeroCasuale) {
-            console.log("Troppo grande")
-        } else {
-            console.log("Troppo piccolo")
+    if (isNaN(numeroInserito)) {
+        console.log("Inserisci un numero valido")
+    } else {
+        if (numeroInserito > numeroCasuale) {
+            console.log("Il numero '" + numeroInserito + "' è troppo grande")
+            tentativi += 1
+        } else if (numeroInserito < numeroCasuale) {
+            console.log("Il numero '" + numeroInserito + "' è troppo piccolo")
+            tentativi += 1
+        } else if (numeroInserito === numeroCasuale) {
+            console.log("Bravo, hai vinto!")
+            indovinato = true
+            document.querySelector("#guess").disabled = true;
+            document.querySelector("#guessBtn").disabled = true;
         }
-
-        i++
     }
-
-
-    if(j === 1){
-        console.log("Bravo, hai vinto")
-    }
-    else{
-        console.log("Mi dispiace, hai finito tutti i tentativi. Il numero era " + numeroCasuale)
+    if (tentativi === 5 && !indovinato) {
+        console.log("Hai esaurito i tentativi, hai perso!")
+        document.querySelector("#guess").disabled = true;
+        document.querySelector("#guessBtn").disabled = true;
     }
 }
+
+let tentativi = 0
+let numeroCasuale = getRandomInt()
+let numeroInserito
+let indovinato = false
+let userField = document.querySelector("#guess")
+let btn = document.querySelector("#guessBtn")
+btn.addEventListener("click", handleClick)
